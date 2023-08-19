41d127c5850d5b508057f9047e1cefd4e45e1f3a
aurelie661 <boutaurel59@gmail.com>
2023-08-19 11:41:02 +0200

    initialize manager

---
+++ Bank/src/main/java/org/example/utils/DatabaseManager.java
@@ -0,0 +1,27 @@
+package org.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    public static Connection getPostgresSQLConnection(){
+
+        String dbUrl = "jdbc:postgresql://localhost:5432/Bank";
+        String userName = "postgres";
+        String userPassword = "Wazemmeslille59!!!";
+
+        try{
+            Connection conn = DriverManager.getConnection(dbUrl,userName,userPassword);
+            if(conn != null){
+                System.out.println("Connexion OK !\n");
+            }else{
+                System.out.println("Connexion ECHOUE !\n");
+            }
+            return conn;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}

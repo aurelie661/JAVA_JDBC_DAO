66e96db3707f4266c433f2d58aaa2a709d7894da
aurelie661 <boutaurel59@gmail.com>
2023-08-19 17:19:26 +0200

    todo DAO && SERVICE bank_account

--- Bank/src/main/java/org/example/dao/BankAccountDAO.java
+++ Bank/src/main/java/org/example/dao/BankAccountDAO.java
@@ -1,10 +1,14 @@
 package org.example.dao;
 
 import org.example.entity.BankAccount;
+import org.example.entity.Customer;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class BankAccountDAO extends BaseDAO<BankAccount> {
 
@@ -15,26 +19,61 @@ public class BankAccountDAO extends BaseDAO<BankAccount> {
 
     @Override
     public boolean save(BankAccount element) throws SQLException {
-        return false;
+        request ="INSERT INTO bank_account (balance,customer_id,operation_id) VALUES (?,?,?)";
+        statement = _connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
+        statement.setDouble(1, element.getBalance());
+        statement.setObject(2, element.getCustomer().getId());
+        statement.setObject(3, element.getOperation().getOperationStatus());
+        int nbRow = statement.executeUpdate();
+        resultSet = statement.getGeneratedKeys();
+        if(resultSet.next()){
+            element.setId(resultSet.getInt(1));
+        }
+        return nbRow == 1;
     }
 
     @Override
     public boolean update(BankAccount element) throws SQLException {
-        return false;
+        request = "UPDATE bank_account SET balance = ? WHERE id = ?";
+        statement = _connection.prepareStatement(request, Statement.RETURN_GENERATED_KEYS);
+        statement.setDouble(1,element.getBalance());
+        statement.setInt(2, (int) element.getId());
+        int row = statement.executeUpdate();
+        return row == 1;
     }
 
     @Override
     public boolean delete(BankAccount element) throws SQLException {
-        return false;
+        request = "DELETE FROM bank_account WHERE id = ?";
+        statement = _connection.prepareStatement(request);
+        statement.setInt(1, (int) element.getId());
+        int row = statement.executeUpdate();
+        return row == 1;
     }
 
     @Override
     public BankAccount get(int id) throws SQLException {
-        return null;
+        BankAccount bankAccount = null;
+        request = "SELECT * FROM bank_account WHERE id = ?";
+        statement = _connection.prepareStatement(request);
+        statement.setInt(1, id);
+        resultSet = statement.executeQuery();
+        if(resultSet.next()){
+            bankAccount = new BankAccount(resultSet.getInt("id"));
+        }
+        return bankAccount;
     }
 
     @Override
     public List<BankAccount> getAll() throws SQLException {
-        return null;
+        List<BankAccount> bankAccounts = new ArrayList<>();
+        request = "SELECT * FROM bank_account";
+        statement = _connection.prepareStatement(request);
+        resultSet = statement.executeQuery();
+        while (resultSet.next()){
+            BankAccount bankAccount = new BankAccount(resultSet.getInt("id"));
+            bankAccounts.add(bankAccount);
+        }
+        return bankAccounts;
     }
 }
